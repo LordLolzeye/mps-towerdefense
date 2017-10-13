c1dd0e9b74dc687ea5852ec0c83f525335ad8798
Tudorut Robert <almantud@yahoo.com>
2017-10-13 19:29:23 +0300

    Initial Commit
    added initial structure

---
+++ src/mps/hawks/project/Main.java
@@ -0,0 +1,17 @@
+package mps.hawks.project;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+	@Override
+    public void onEnable() {
+		System.out.println("First commit");
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+	
+}
